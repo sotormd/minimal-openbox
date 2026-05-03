24a667c3df7b80bf3de183a537a64fa95f566abb
vrm <sotormd@proton.me>
2026-05-03 10:54:52 +0530

    refactor: cleanup

--- flake.nix
+++ flake.nix
@@ -1,20 +1,4 @@
 {
   description = "very minimal openbox for my nixos needs";
-
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-  outputs =
-    { self, ... }@inputs:
-    {
-      nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          self.nixosModules.coffee
-        ];
-      };
-
-      nixosModules.coffee = _: { imports = [ ./wm.nix ]; };
-
-    };
+  outputs = _: { nixosModules.coffee = _: { imports = [ ./wm.nix ]; }; };
 }
